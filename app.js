293e406b3bb241b8a875a953e9c77a69dba146be
André Gabos <andre.gabos@gmail.com>
2021-03-14 13:54:01 -0300

    "Adição de database, e preparação para deploy"

--- app.js
+++ app.js
@@ -1,26 +1,41 @@
 const express = require("express");
+const mongoose = require("mongoose")
 const ejs = require("ejs");
 const kebabCase = require('lodash.kebabcase');
+
+const app = express();
+app.set('view engine', 'ejs');
+app.use(express.urlencoded({extended: true}));
+app.use(express.static("public"));
+
+const uri = "mongodb+srv://angDB:7b0oAwwnZAAr9d8l@cluster0.ppgqt.mongodb.net/blogDB?retryWrites=true&w=majority"
+
+mongoose.connect(uri, {useNewUrlParser: true, useUnifiedTopology: true});
+
+const postSchema = new mongoose.Schema({
+  title: String,
+  content: String
+});
+
+const Post = new mongoose.model('post', postSchema);
 
 const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
 const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
 const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
 
-const app = express();
-
-app.set('view engine', 'ejs');
-
-let posts = [];
-
-app.use(express.urlencoded({extended: true}));
-app.use(express.static("public"));
 
 app.get("/", (req, res) => {
-  res.render("home", {
-    homeC: homeStartingContent,
-    homePosts: posts
-  });
 
+  Post.find({}, (err, foundPosts) => {
+    if (!err) {
+      res.render("home", {
+        homeC: homeStartingContent,
+        homePosts: foundPosts
+      })
+    } else {
+      res.render(err)
+    }
+  });
 });
 
 app.get("/about", (req, res) => {
@@ -40,35 +55,37 @@ app.get("/compose", (req, res) => {
 });
 
 app.post("/compose", (req, res) => {
-  let postTitle = req.body.composeTitle;
-  let postBody = req.body.composeBody;
+  
+  let compPost = new Post({
+    title: req.body.composeTitle,
+    content: req.body.composeBody
+  });
 
-  let post = {
-    title: postTitle,
-    body: postBody
-  };
-
-  posts.push(post);
-
-  res.redirect("/")
-});
-
-app.get("/posts/:postLog", (req, res) => {
-  const requestedTitle = kebabCase(req.params.postLog);
-  posts.forEach((post) => {
-    let postTitle = kebabCase(post.title)
-    if (postTitle === requestedTitle) {
-      res.render("post", {
-        newPostTitle: post.title,
-        newPostParagraph: post.body
-      }
-    )};
+  compPost.save(() => {
+    res.redirect("/")
   });
 });
 
+app.get("/posts/:postId", (req, res) => {
+  const requestedId = req.params.postId;
 
+  Post.findById(requestedId, (err, post) => {
+    if (!err) {
+      res.render("post", {
+        postTitleN: post.title,
+        postContentN: post.content
+      });
+    } else {
+      res.render(err);
+    }
+  });
+});
 
+let port = process.env.PORT;
+if (port == null || port == "") {
+    port = 3000;
+};
 
-app.listen(3000, function() {
-  console.log("Server started on port 3000");
+app.listen(port, () => {
+    console.log("Running on port 3000")
 });
